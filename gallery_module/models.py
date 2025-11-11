826df9986baa24d64c343ba15a8bfa187ad0e42e
mohsen <mohsenmansourikhah@gmail.com>
2025-11-11 17:29:57 +0330

    add gallery module models urls admin and view

--- gallery_module/models.py
+++ gallery_module/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+class Gallery(models.Model):
+    title = models.CharField(max_length=200, verbose_name="عنوان عکس")
+    image = models.ImageField(upload_to="uploads/gallery/", verbose_name="تصویر")
+    is_delete = models.BooleanField(verbose_name="حذف شده / نشده")
+    is_active = models.BooleanField(default=True, verbose_name="فعال / غیرفعال")
+
+    def __str__(self):
+        return self.title
+
+
+    class Meta:
+        verbose_name = "تصویر گالری"
+        verbose_name_plural = "تصویر گالری"
--- gallery_module/views.py
+++ gallery_module/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.views.generic.list import ListView
+from .models import Gallery
 
 # Create your views here.
+
+class GalleryView(ListView):
+    model = Gallery
+    template_name = "gallery_module/gallery_page.html"
+    context_object_name = "galleries"
+    paginate_by = 5
+
